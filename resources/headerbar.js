7d0a50b3c751c0b0c9679f4e3a4cbc519c2aad5a
tres <59184373+pybash@users.noreply.github.com>
2021-01-29 13:27:25 -0500

    Initial Commit

---
+++ resources/headerbar.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    let logobutton = document.getElementById("logobutton")
+    logobutton.addEventListener("click" , () => {
+        window.location = "./index.html"
+    })
+})
